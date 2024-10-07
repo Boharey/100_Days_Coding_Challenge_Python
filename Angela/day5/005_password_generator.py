bfb5778f565ef00a3f58a6081686f4e32f284c74
Boharey <utsavbohara4@gmail.com>
2024-10-07 09:52:49 +0545

    understood the working of list and print statement's argument : end

---
+++ Angela/day5/005_password_generator.py
@@ -0,0 +1,5 @@
+#day 5 of the 100 days python challenge series of angela course
+
+print("Welcome to password generator")
+no_of_letters = input("How many letters would you like in your password ?")
+no_of_symbols = input("How many symbols would you like in your password ?")
---
+++ Angela/day5/lists.py
@@ -0,0 +1,4 @@
+fruits = ["apple","peach","pear"]
+for fruit in fruits:
+  print(fruit,end=" : ")
+  print(len(fruit))
